e024b5a45561bd70694a0f31973847af77cf0ee3
amonge20 <amonge20@jaumebalmes.net>
2023-04-13 20:00:18 +0200

    Practica 17 hecha

--- JaumeBalmes_Aitor/juegos/models.py
+++ JaumeBalmes_Aitor/juegos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Crear BBDD de estudiantes
+class Students(models.Model):
+    name = models.CharField(max_length = 50)
+    surname = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    course = models.CharField(max_length = 100)
+    birthdate = models.IntegerField()
+    population = models.CharField(max_length = 50)
+
+# Crear BBDD de profesores
+class Teachers(models.Model):
+    name = models.CharField(max_length = 50)
+    surname = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    subject = models.CharField(max_length = 50)
+    birthdate = models.IntegerField()
+    population = models.CharField(max_length = 50)
