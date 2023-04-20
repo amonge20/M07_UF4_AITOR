37b6614bd72510b4329903a4622c7ea16fce23f6
amonge20 <amonge20@jaumebalmes.net>
2023-04-20 20:54:04 +0200

    Update

--- JaumeBalmes_Aitor/juegos/urls.py
+++ JaumeBalmes_Aitor/juegos/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('teacher/<str:pk>/', views.teacher, name='teacher'),
     path('student/<str:pk>/', views.student, name='student'),
     path('user-form/', views.user_form, name='user_form'),
+    path('alumneForm/', views.alumneForm, name='alumneForm'),
+    path('profeForm/', views.profeForm, name='profeForm'),
+    path('updateAlumne', views.update_alumne, name='update_alumne'),
+    path('updateProfe', views.update_profe, name='update_profe'),
 ]
